a73bf5b7575555fbe9770c1c9ae92897a036e2b0
Alina Salinas <142938656+Serenity-24@users.noreply.github.com>
2024-02-27 18:22:04 +0000

    Tue, Feb 27, 2024, 10:22 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -13,6 +13,8 @@ function blurring(){
         clearInterval(int)
     }
     loadText.innerText = `$(load)%`
+
+    
     loadText.style.opacity = 1 - load/100
     bg.style.filter = `blur(${30 -(load/100)*30}px)`
 }
