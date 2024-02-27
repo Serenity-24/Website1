7e30e358d6e359f9106ad4902dc99b2cf0332eba
Alina Salinas <142938656+Serenity-24@users.noreply.github.com>
2024-02-27 17:59:02 +0000

    Tue, Feb 27, 2024, 9:59 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,13 @@
 loadText = document.querySelector('.loading-text)
+bg = document.querySelector('.bg')
 
-load = 90
+load = 0
 
-loadText.innerText = `$(load)%`
-loadText.style.opacity
+int = setInterval(blurring,40)
 
+function blurring(){
+    load++
+    loadText.innerText = `$(load)%`
+loadText.style.opacity = 0.5
+bg.style.filter = "blur(10px)"
+}
